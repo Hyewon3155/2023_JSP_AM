712e30637e3f8f78d1e79e40cd8774d23ef7a756
Hyewon3155 <lucybeauty1211@gmail.com>
2023-04-10 22:03:30 +0900

    로그아웃 기능 구현

--- src/main/java/com/koreaIT/java/am/MainPageServlet.java
+++ src/main/java/com/koreaIT/java/am/MainPageServlet.java
@@ -7,12 +7,23 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 @WebServlet("/home/main")
 public class MainPageServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		
+		int loginedMemberId = -1;
+		
+		if(session.getAttribute("loginedMemberId") != null) {
+			// null인 경우, 값이 안 들어있음, 로그인을 하지 않음 
+			loginedMemberId = (int)session.getAttribute("loginedMemberId");
+			//session의 타입은 Object, 즉, 형 변환을 해주어야 함 
+		}
+		request.setAttribute("loginedMemberId", loginedMemberId);
 		request.getRequestDispatcher("/jsp/home/main.jsp").forward(request, response);
 	}
 }
--- src/main/webapp/jsp/home/main.jsp
+++ src/main/webapp/jsp/home/main.jsp
@@ -1,5 +1,8 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%
+	int loginedMemberId = (int) request.getAttribute("loginedMemberId");
+%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,8 +11,26 @@
 </head>
 <body>
 	<h1>메인 페이지</h1>
-	
+	<!--로그인을 하지 않은 경우(로그인한 아이디의 정보가 없는 경우)
+	    로그인과 회원가입이 나타남   -->
+	<%
+	if(loginedMemberId == -1){
+    %>
+		<div><a href="../member/login">로그인</a></div>
+		<div><a href="../member/join">회원가입</a></div>
+    <%
+	}
+    %>
+    <!-- 로그인을 한 경우(로그인한 아이디의 정보가 있는 경우)
+         로그아웃이 나타남  -->
+    <%
+    if(loginedMemberId != -1){
+    %>	
+    	<div><a href="../member/doLogout">로그아웃</a></div>
+    <%
+    }
+    %>
 	<div><a href="../article/list">게시물 리스트</a></div>
-	<div><a href="../article/memberjoin">회원가입</a></div>
+	
 </body>
 </html>
