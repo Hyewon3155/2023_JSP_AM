c66f79189b98a9e264931f05217a42969e2ab13a
Hyewon3155 <125236708+Hyewon3155@users.noreply.github.com>
2023-04-12 05:43:24 +0900

    Update ArticleModifyServlet.java

--- src/main/java/com/koreaIT/java/am/ArticleModifyServlet.java
+++ src/main/java/com/koreaIT/java/am/ArticleModifyServlet.java
@@ -46,18 +46,22 @@ public class ArticleModifyServlet extends HttpServlet {
 			if (session.getAttribute("loginedMemberId") != null) {
 				loginedMemberId = (int) session.getAttribute("loginedMemberId");
 			}
-			
+			// 현재 로그인을 한 상태이면 loginedMemberId 변수에 로그인한 아이디를 가져옴(세션으로부터)
 			if(loginedMemberId == -1) {
 				response.setContentType("text/html; charset=UTF-8;");
 				response.getWriter().append(String.format("<script>alert('로그인 후 이용해주세요'); location.replace('../member/login');</script>"));
 				return;
 			}
-			
+			// 현재 로그인을 한 상태가 아니면(값이 변수에 들어오지 못해, 초기값인 -1로 설정되어 있음) 
+			// 로그인 후 이용해 달라는 문구 
 			if ((int) articleRow.get("memberId") != loginedMemberId) {
+				// URL로 검색해서 들어왔을 때, 파라미터로 직접 memberId를 입력한 경우
 				response.setContentType("text/html; charset=UTF-8;");
 				response.getWriter().append(String.format("<script>alert('해당 게시물에 대한 권한이 없습니다'); location.replace('detail?id=%d');</script>", id));
 				return;
 			}
+			// 현재 로그인한 아이디와 작성한 사람의 Id가 동일하지 않으면, (게시물의 작성자와 로그인한 사람의 아이디를 비교)
+			// 해당 게시물에 대한 권한이 없다고 출력
 			
 			request.getRequestDispatcher("/jsp/article/modify.jsp").forward(request, response);
 			
